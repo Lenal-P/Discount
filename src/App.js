693e190c4dbd61b1e439dcbf87579083f514b754
Lenal-P <lenalpiggy@gmail.com>
2024-03-05 14:56:00 +0700

    Sum history

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
   const [history, setHistory] = useState([]);
 
   const handleClick = (value) => {
@@ -19,20 +19,24 @@ function App() {
         setHistory([...history, finalResult]);
       }
       // Reset ô input
-      setResult('');
+      setResult("");
     } catch (error) {
-      setResult('Error');
+      setResult("Error");
     }
   };
-  
+
   const handleClear = () => {
-    setResult('');
+    setResult("");
   };
 
   const clearHistory = () => {
     setHistory([]);
   };
-  
+
+  const handleBackspace = () => {
+    setResult(result.slice(0, -1));
+  };
+
   const handleDiscount = (percentage) => {
     // Hàm tính giảm giá
     try {
@@ -45,46 +49,79 @@ function App() {
         return finalResult.toString();
       });
       // Reset ô input
-      setResult('');
+      setResult("");
     } catch (error) {
-      setResult('Error');
+      setResult("Error");
     }
   };
-  
 
   const handleHistoryClick = (value) => {
     // Chuyển đổi giá trị từ chuỗi sang số
     const numericValue = parseFloat(value);
-  
     // Kiểm tra xem giá trị đã được chuyển đổi thành số hay không
     if (!isNaN(numericValue)) {
       // Nếu là số, đặt giá trị mới cho result
-      setResult(numericValue);
+      setResult(result + numericValue);
     } else {
-      setResult('Error');
+      setResult("Error");
     }
+  };
+
+  const handleSumHistory = () => {
+    const sum = history.reduce((acc, curr) => acc + parseFloat(curr), 0);
+    setResult(sum.toString());
   };
 
   return (
     <div className="calculator">
       <input className="result" type="text" value={result} disabled />
       <div className="buttons">
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('+')} style={{ backgroundColor: '#FFE4C9' }}>+</button>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('-')} style={{ backgroundColor: '#FFE4C9' }}>-</button>
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('*')} style={{ backgroundColor: '#FFE4C9' }}>x</button>
-        <button onClick={() => handleClick('.')} style={{ backgroundColor: '#B7E5B4' }}>.</button>
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={handleCalculate} style={{ backgroundColor: '#B7E5B4' }}>=</button>
-        <button onClick={() => handleClick('/')} style={{ backgroundColor: '#FFE4C9' }}>:</button>
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button
+          onClick={() => handleClick("+")}
+          style={{ backgroundColor: "#FFE4C9" }}
+        >
+          +
+        </button>
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button
+          onClick={() => handleClick("-")}
+          style={{ backgroundColor: "#FFE4C9" }}
+        >
+          -
+        </button>
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button
+          onClick={() => handleClick("*")}
+          style={{ backgroundColor: "#FFE4C9" }}
+        >
+          x
+        </button>
+        <button
+          onClick={() => handleClick(".")}
+          style={{ backgroundColor: "#B7E5B4" }}
+        >
+          .
+        </button>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button
+          onClick={handleCalculate}
+          style={{ backgroundColor: "#B7E5B4" }}
+        >
+          =
+        </button>
+        <button
+          onClick={() => handleClick("/")}
+          style={{ backgroundColor: "#FFE4C9" }}
+        >
+          :
+        </button>
         <button onClick={() => handleDiscount(10)}>10%</button>
         <button onClick={() => handleDiscount(15)}>15%</button>
         <button onClick={() => handleDiscount(20)}>20%</button>
@@ -95,8 +132,28 @@ function App() {
         <button onClick={() => handleDiscount(45)}>45%</button>
         <button onClick={() => handleDiscount(50)}>50%</button>
         <button onClick={() => handleDiscount(55)}>55%</button>
-        <button onClick={handleClear} style={{ backgroundColor: '#FF6868' }}>Cls</button>
-        <button onClick={clearHistory} style={{ backgroundColor: '#FF6868' }}>ClsHis</button>
+        <button onClick={() => handleDiscount(60)}>60%</button>
+        <button onClick={() => handleDiscount(65)}>65%</button>
+        <button
+          onClick={handleSumHistory}
+          style={{ backgroundColor: "#ADD8E6" }}
+        >
+          SumHis
+        </button>
+
+        <button
+          onClick={handleBackspace}
+          style={{ backgroundColor: "#FF6868" }}
+        >
+          ←
+        </button>
+
+        <button onClick={handleClear} style={{ backgroundColor: "#FF6868" }}>
+          Cls
+        </button>
+        <button onClick={clearHistory} style={{ backgroundColor: "#FF6868" }}>
+          ClsHis
+        </button>
       </div>
 
       {/* Lịch sử tính toán */}
@@ -104,9 +161,13 @@ function App() {
         <h3>History</h3>
         <ul>
           {history.map((item, index) => (
-            <li key={index} onClick={() => handleHistoryClick(item)} style={{ cursor: 'pointer' }}>
+            <li
+              key={index}
+              onClick={() => handleHistoryClick(item)}
+              style={{ cursor: "pointer" }}
+            >
               {/* Hiển thị lịch sử tính toán với định dạng số */}
-              {new Intl.NumberFormat('vi-VN').format(item)}
+              {new Intl.NumberFormat("vi-VN").format(item)}
             </li>
           ))}
         </ul>
